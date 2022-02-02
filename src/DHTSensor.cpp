d00571fc798a2715532423d09936e7277ce86c25
Dmitrii Kurskov <dmitrii@kurskov.ru>
2022-02-02 16:21:12 +0300

    Added comments

--- src/DHTSensor.cpp
+++ src/DHTSensor.cpp
@@ -19,7 +19,7 @@ float DHTSensor::getTemp(bool units) {
     if (units) {
         return temp;
     } else {
-        return temp * 9 / 5 +32; // Celsius to fahrenheit 
+        return temp * 1.8 + 32; // Celsius to fahrenheit 
     }
 }
 
@@ -30,37 +30,44 @@ float DHTSensor::getHum() {
     return hum;
 }
 
-void setPin(uint8_t pin) {
+void DHTSensor::setPin(uint8_t pin) {
     _pin = pin;
     pinMode(_pin, OUTPUT);
     digitalWhite(_pin, HIGH);
 }
 
-uint8_t readData() {
+uint8_t DHTSensor::readData() {
     uint8_t testLine;
+
+    // Empties last data
+    for (uint8_t i = 0; i < 5; i++) {
+        _data[i] = 0;
+    }
     
-    //
+    // Send out start signal
+    // LOW > 18 ms, HIGH 20-40 us.
     digitalWrite(_pin, LOW);
     delay(18);
     digitalWrite(_pin, HIGH);
-    //
     delayMicroseconds(30);
-    //
-    pinMode(_pin, INPUT);
-    //
-    delayMicroseconds(40);
     
+    // Set pin as input 
+    // and delay half of first response signal.
+    pinMode(_pin, INPUT);
+    delayMicroseconds(40);
+    // Check 0.
     if ( digitalRead(_pin) ) {
         return DHT_ERROR_INIT_0;
     }
-
+    // delay to half of second response signal.
     delayMicroseconds(80);
-
+    // Check 1.
     if ( !digitalRead(_pin) ) {
         return DHT_ERROR_INIT_1;
     }
-
     
+    // Data reception 
+
 
     return DHT_STATUS_OK
 }
